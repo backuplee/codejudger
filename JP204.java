9f3e6e7d6b0b5a0b76476bc558c3647d4e70abac
backuplee <backuplee8@gmail.com>
2022-09-22 17:12:40 +0800

    20220922-1712

--- JP204.java
+++ JP204.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class JP204 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
         int n1, n2, count;
@@ -22,14 +22,14 @@ public class JP204 {
         if ((n1 > 100) | (n1 < 1) | (n2 > 100) | (n2 < 1)) {
             System.out.println("error");
         } else {
-            for (int i = n1; i < n2; i++) {
+            for (int i = n1; i <= n2; i++) {
                 count = 0;
-                for (int j = 1; j < i; j++) {
+                for (int j = 1; j <= i; j++) {
                     if (i % j == 0) {
                         count++;
                     }
                 }
-                if (count < 3) {
+                if ((count < 3) & (i != 1)) {
                     System.out.println(i);
                 }
             }
